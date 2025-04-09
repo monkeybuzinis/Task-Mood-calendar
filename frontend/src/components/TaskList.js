75963e5e19d0f2f11415e4dc37e2b66333663b7f
monkeybuzinis <kakale2602@gmail.com>
2025-04-09 14:09:51 -0700

    while fixing the "delete task" problem, cs has generated this change but maybe the function is not used, I haven't checked

--- frontend/src/components/TaskList.js
+++ frontend/src/components/TaskList.js
@@ -15,4 +15,16 @@ const TaskList = ({ tasks, onToggle, onDelete }) => (
   </ul>
 );
 
+const renderDates = (tasks) => {
+  const dates = Object.keys(tasks); // Assuming tasks is an object with dates as keys
+  return dates.map(date => {
+    const hasTasks = tasks[date] && tasks[date].length > 0; // Check if there are tasks for the date
+    return (
+      <div key={date} style={{ backgroundColor: hasTasks ? 'lightgreen' : 'transparent' }}>
+        {date}
+      </div>
+    );
+  });
+};
+
 export default TaskList;
